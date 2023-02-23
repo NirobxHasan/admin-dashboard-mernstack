4a0ad67023c66de332c07e0cadd5ab923ec935f4
nirobxhasan <thenirobhasan@gmail.com>
2023-02-23 18:10:48 +0600

    client: rtk query setUo

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import globalReducer from 'state'
 import { Provider } from 'react-redux';
 
+import {api} from 'state/api'
+import { setupListeners } from '@reduxjs/toolkit/query';
+ 
 const store = configureStore({
   reducer:{
     global: globalReducer,
-  }
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware)
 })
-
+setupListeners(store.dispatch)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -3,10 +3,15 @@ import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from 'components/Navbar'
 import Sidebar from 'components/Sidebar'
+import { useSelector } from 'react-redux'
 
 function Layout() {
   const isNonMobile = useMediaQuery("(min-width: 600px)")
   const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  
+  const userId = useSelector((state)=> state.global.userId);
+
+
   return (
     <Box display={isNonMobile ? "flex":"block"} height="100%" width="100%">
       <Sidebar
