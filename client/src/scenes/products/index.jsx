5b279f4b134b2126f1219d240c98c9773179e17c
nirobxhasan <thenirobhasan@gmail.com>
2023-02-27 15:46:25 +0600

    Customer Module added

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -7,7 +7,7 @@ import { useGetProductsQuery } from 'state/api'
 function Products() {
     const {data, isLoading, } = useGetProductsQuery()
     console.log("🚀 ~ file: index.jsx:9 ~ Products ~ data:", data)
-    const isNonMobile = useMediaQuery('(min-width: 100px)')
+    const isNonMobile = useMediaQuery('(min-width: 1000px)')
   return (
     <Box m="1.5rem 2.5rem">
         <Header title={'Products'} subtitle={'See your list of products'}/>
--- server/routes/client.js
+++ server/routes/client.js
@@ -1,5 +1,7 @@
-import express from 'express'
-import {getProducts} from '../controllers/client.js'
+import express from "express";
+import { getCustomers, getProducts } from "../controllers/client.js";
 const router = express.Router();
-router.get("/products", getProducts)
+router.get("/products", getProducts);
+router.get("/customers", getCustomers);
+
 export default router;
