01087e663be1dbef38c11d775946888fef8f305f
nirobxhasan <thenirobhasan@gmail.com>
2023-03-01 15:23:41 +0600

    Transactions added

--- server/index.js
+++ server/index.js
@@ -1,53 +1,50 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import clientRoutes from './routes/client.js'
-import generalRoutes from './routes/general.js'
-import managementRoutes from './routes/management.js'
-import salesRoutes from './routes/sales.js'
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 /* DATA IMPORT */
-import User from './models/User.js'
-import Product from './models/Products.js'
-import ProductStat from './models/ProductStat.js'
-import {dataUser, dataProduct, dataProductStat} from './data/index.js'
 
 /* CONFIGARATION */
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy:'cross-origin' }));
-app.use(morgan("common"))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(cors())
-
-
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 /* Routes */
-app.use('/client', clientRoutes)
-app.use('/general', generalRoutes)
-app.use('/management', managementRoutes)
-app.use('/sales', salesRoutes)
-
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
 
 /* MONGOOSE SETUP */
 const port = process.env.PORT || 9000;
 
-mongoose.connect(process.env.MONGO_URL,{
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=>{
-    app.listen(port, ()=>{
-        console.log(`Server port: ${port}`)
-        /* ONLY ADD DATA ONE TIME */
-        // User.insertMany(dataUser)
-        // Product.insertMany(dataProduct)
-        // ProductStat.insertMany(dataProductStat)
-    })
-}).catch((error)=> console.log(`${error} did not connect`))
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server port: ${port}`);
+      /* ONLY ADD DATA ONE TIME */
+      // User.insertMany(dataUser)
+      // Product.insertMany(dataProduct)
+      // ProductStat.insertMany(dataProductStat)
+      //   Transaction.insertMany(dataTransaction);
+    });
+  })
+  .catch((error) => console.log(`${error} did not connect`));
